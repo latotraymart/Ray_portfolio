41a93125b4c8574f0a5368907864f2e27bcda49f
rpgjr <rodrigo21geneta@gmail.com>
2023-09-02 15:17:44 +0800

    Hero Section added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='block md:flex columns-1 md:columns-2 items-center justify-center gap-10 py-20 px-10 lg:px-24 xl:px-48'>
+      <div id='hero-profile-box' className='flex justify-center'>
+        <div id='hero-profile'></div>
+      </div>
+      <div className='text-white text-center md:text-left mt-10 md:mt-0'>
+        <p className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold mb-1'>Hi! I'm Rodrigo</p>
+        <p className='text-lg md:text-xl xl:text-2xl font-semibold mb-1'>Web Developer</p>
+        <p className='text-sm text-left' id='intro-text'>I'm an aspiring Full Stack Web Developer who's eager to learn and to grow in the field of Web Development. I'm passionate and enthusiastic in becoming a Developer and willing to explore more in the field of IT.</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import { FaBrain, FaEnvelope, FaFilePdf, FaFolderOpen, FaHouse } from "react-icons/fa6";
+import React, { useState } from 'react';
+import { FaBrain, FaEnvelope, FaFilePdf, FaFolderOpen, FaHouse, FaAlignJustify } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  
+  const [menuText, setMenuText] = useState('rod.dev');
+
+  const menuHover = (text) => {
+    setMenuText(text);
+  }
+
   return (
     <nav id='navbar'>
       <div id='navbar-design'>
         <div className='flex justify-between items-center'>
-        <div className='nav-items'>
+          <div className='nav-items' id='nav-title'>
             <NavLink to='/'>
-              <span className='nav-text'>Hello!</span>
-              <span className='hidden sm:block font-semibold hover:scale-x-110 duration-300'>rod.dev</span>
+              {/* <span className='nav-text'>Hello!</span> */}
+              <span className='hidden sm:block font-semibold hover:scale-x-110 duration-300'>{menuText}</span>
             </NavLink>
           </div>
-          <div className='nav-items'>
+          <div className='nav-items' onMouseEnter={() => menuHover('Home')} onMouseLeave={() => menuHover('rod.dev')}>
             <NavLink to='/'>
               <span className='nav-text'>Home</span>
               <FaHouse className='nav-icons' />
             </NavLink>
           </div>
-          <div className='nav-items'>
+          <div className='nav-items' onMouseEnter={() => menuHover('Experiences')} onMouseLeave={() => menuHover('rod.dev')}>
             <NavLink to=''>
               <span className='nav-text'>Experiences</span>
               <FaFolderOpen className='nav-icons' />
             </NavLink>
           </div>
-          <div className='nav-items'>
+          <div className='nav-items' onMouseEnter={() => menuHover('Skills')} onMouseLeave={() => menuHover('rod.dev')}>
             <NavLink to=''>
               <span className='nav-text'>Skills</span>
               <FaBrain className='nav-icons' />
             </NavLink>
           </div>
-          <div className='nav-items'>
+          <div className='nav-items' onMouseEnter={() => menuHover('Contacts')} onMouseLeave={() => menuHover('rod.dev')}>
             <NavLink to=''>
               <span className='nav-text'>Contacts</span>
               <FaEnvelope className='nav-icons' />
             </NavLink>
           </div>
-          <div className='nav-items'>
+          <div className='nav-items' onMouseEnter={() => menuHover('Resume')} onMouseLeave={() => menuHover('rod.dev')}>
             <NavLink to=''>
               <span className='nav-text'>Resume</span>
               <FaFilePdf className='nav-icons' />
